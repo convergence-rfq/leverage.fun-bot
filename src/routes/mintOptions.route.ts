030b2b5f5060073aea41f47173bcab100b8d6a37
raghav-rama <hackerer528@gmail.com>
2024-11-28 09:48:33 +0530

    fix: deploy script and test route

--- src/routes/mintOptions.route.ts
+++ src/routes/mintOptions.route.ts
@@ -1,8 +1,12 @@
-import { startMintingOptions } from '../controllers/mintOptions.controller.js';
+import {
+  startMintingOptions,
+  getMintOptions,
+} from '../controllers/mintOptions.controller.js';
 import { Router } from 'express';
 
 const router = Router();
 
+router.get('/', getMintOptions);
 router.post('/', startMintingOptions);
 
 export default router;
