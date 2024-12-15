003c42fa9f64dfca29e05691b3ed635479e3dc1a
raghav-rama <hackerer528@gmail.com>
2024-12-15 17:23:35 +0530

    feat: add vault and auction program

--- src/utils/programUtils.ts
+++ src/utils/programUtils.ts
@@ -1,5 +1,7 @@
 import * as anchor from '@coral-xyz/anchor';
 import { EuroPrimitive } from '../types/euro_primitive.js';
+import { Vaults } from '../types/vaults.js';
+import { OptionAuction } from '../types/option_auction.js';
 import Config from '../config.js';
 
 export async function getProgram(
@@ -14,4 +16,27 @@ export async function getProgram(
     console.error(error);
     throw error;
   }
+}
+
+export async function getVaultsProgram(
+  provider: anchor.Provider,
+): Promise<anchor.Program<Vaults>> {
+  try {
+    const pid = Config.VAULTS_PROGRAM_ID;
+    const idl = await anchor.Program.fetchIdl<Vaults>(pid, provider);
+    const program = new anchor.Program<Vaults>(idl!, provider);
+    return program;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function getAuctionProgram(
+  provider: anchor.Provider,
+): Promise<anchor.Program<OptionAuction>> {
+  const pid = Config.AUCTION_PROGRAM_ID;
+  const idl = await anchor.Program.fetchIdl<OptionAuction>(pid, provider);
+  const program = new anchor.Program<OptionAuction>(idl!, provider);
+  return program;
 }
